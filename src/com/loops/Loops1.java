627f9026ab4223cf9773812ac3699211cd06f0f9
sivaraman1998 <ramansiva1998@gmail.com>
2022-11-13 20:43:52 +0530

    first commit

---
+++ src/com/loops/Loops1.java
@@ -0,0 +1,26 @@
+package com.loops;
+
+import java.util.Iterator;
+
+public class Loops1 {
+	
+	public static void main(String[] args) {
+	 	
+      for (int i =1 ; i<=5; i++) {
+    	  
+    	  for (int j =i+1; j<=i+1;j++) {
+    		  System.out.print(" * ");
+    	  }
+    	 
+			
+		}
+    	   System.out.println(" ");
+		
+	}	
+		}	
+	 
+
+
+
+	
+
